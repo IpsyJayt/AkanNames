e899f2229ef030f896be5eed3702b51d7248cb25
IpsyJayt <ipsyjayt@gmail.com>
2021-11-14 20:11:57 +0300

    add if statements to check validity of day,month and year inputs

--- scripts.js
+++ scripts.js
@@ -5,5 +5,15 @@ var monthOfBirth = parseInt(document.getElementById("month").value);
 var yearOfBirth =document.getElementById("year").value;
 var gender = document.getElementById("gender").value;
 
+var cc = parseInt(yearOfBirth.slice(0,2));
+var yy = parseInt(yearOfBirth.slice(2,4));
+
+if(dayOfBIrth<1 || dayOfBIrth>31){
+    alert("Please enter a day between 1 and 31")
+}
+if(monthOfBirth<1 || monthOfBirth>12){
+    alert("Please choose a month between 1 and 12")
+}
+
 
 }
