1223b65d388141a3b05aacf9ea953052872ee24f
IpsyJayt <ipsyjayt@gmail.com>
2021-11-14 19:34:00 +0300

    remove the parseInt on year of birth to allows for slicing

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ function generateAkanName(){
 
 var dayOfBIrth = parseInt(document.getElementById("day").value);
 var monthOfBirth = parseInt(document.getElementById("month").value);
-var yearOfBirth = parseInt(document.getElementById("year").value);
+var yearOfBirth =document.getElementById("year").value;
 var gender = document.getElementById("gender").value;
+
 
 }
